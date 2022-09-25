e2e809d7c7a49d1d036e9d7cee16ea11b75e5b9d
Julen Smith <jsmith@c3r7s4.42urduliz.com>
2022-09-25 19:08:59 +0200

    Shot animation, bullets and face added to hud

--- src/hooks/hook_utils_0.c
+++ src/hooks/hook_utils_0.c
@@ -1,15 +1,6 @@
 #include "../../include/cub3d.h"
 
 
-int manage_mouse_hook_options(t_hook *hk)
-{
-	(void)hk;
-/*	int x[2] = {5,5};
-	int y[2] = {6,6};
-	(void)hk;
-	printf("%d\n", mlx_mouse_get_pos(hk->gr->mlx_win,x,y));
-*/	return (1);
-}
 
 //close window with red botton
 int	close_button(t_hook *hk)
